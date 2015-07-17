12a45379affbad5b0802dc31dc031ab836c5589c
neoremind <notyetfish@hotmail.com>
2015-07-17 22:54:35 +0800

    add delete node

---
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/DeleteNodeInSinglyLinkedList.java
@@ -0,0 +1,29 @@
+package net.neoremind.mycode.argorithm.leetcode;
+
+/**
+ * ClassName: DeleteNodeInSinglyLinkedList <br/>
+ * Function: Write a function to delete a node (except the tail) in a singly linked list, given only access to that
+ * node.
+ * <p/>
+ * Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should
+ * become 1 -> 2 -> 4 after calling your function.
+ * 
+ * @author Zhang Xu
+ */
+public class DeleteNodeInSinglyLinkedList {
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}

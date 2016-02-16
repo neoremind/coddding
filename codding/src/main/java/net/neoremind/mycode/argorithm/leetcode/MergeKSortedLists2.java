edb276e2ab2a97a20e32af26857865e9feeca893
neoremind <notyetfish@hotmail.com>
2016-02-16 13:28:05 +0800

    add merge k sorted lists2

--- codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/MergeKSortedLists2.java
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/MergeKSortedLists2.java
@@ -13,7 +13,13 @@ import net.neoremind.mycode.argorithm.leetcode.support.ListNodeHelper;
 /**
  * Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
  * <p/>
+ * 这种方法用到了堆的数据结构，思路比较难想到，但是其实原理比较简单。
+ * 维护一个大小为k的堆，每次去堆顶的最小元素放到结果中，然后读取该元素的下一个元素放入堆中，重新维护好。
+ * 因为每个链表是有序的，每次又是去当前k个元素中最小的，所以当所有链表都读完时结束，这个时候所有元素按从小到大放在结果链表中。
+ * 这个算法每个元素要读取一次，即是k*n次，然后每次读取元素要把新元素插入堆中要logk的复杂度，所以总时间复杂度是O(nklogk)
+ * 。空间复杂度是堆的大小，即为O(k)。
  * 这个实现在leetcode上打败了71%的java代码。
+ * 本解法没有用到java自带的优先队列PriorityQueue，而是自己实现的。
  *
  * @author zhangxu
  */

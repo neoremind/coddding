fb0675f64faf9923341e8a7aebb3b99e845268d1
neoremind <notyetfish@hotmail.com>
2016-06-13 16:04:49 +0800

    update nio time

--- codding/src/main/java/net/neoremind/mycode/nio/bio/TimeClient.java
+++ codding/src/main/java/net/neoremind/mycode/nio/bio/TimeClient.java
@@ -7,53 +7,53 @@ import java.net.Socket;
 
 public class TimeClient {
 
-	public static void main(String[] args) {
-		int port = 8080;
-		Socket socket = null;
-		BufferedReader in = null;
-		PrintWriter out = null;
-		try {
-			socket = new Socket("127.0.0.1", port);
-			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			out = new PrintWriter(socket.getOutputStream(), true);
-			out.println("GET TIME");
-			System.out.println("Send request to server!");
-			String res = in.readLine();
-			System.out.println("Response is " + res);
-			
-//			Thread.sleep(2000);
-//			
-//			out.println("BAD BAD BAD");
-//			System.out.println("Send request to server!");
-//			res = in.readLine();
-//			System.out.println("Response is " + res);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-			
-			if (out != null) {
-				try {
-					out.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-			
-			if (socket != null) {
-				try {
-					socket.close();
-				} catch (Exception e2) {
-					e2.printStackTrace();
-				}
-			}
-		}
-	}
-	
+    public static void main(String[] args) {
+        int port = 8082;
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("GET TIME");
+            System.out.println("Send request to server!");
+            String res = in.readLine();
+            System.out.println("Response is " + res);
+
+            Thread.sleep(2000);
+
+            out.println("BAD BAD BAD");
+            System.out.println("Send request to server!");
+            res = in.readLine();
+            System.out.println("Response is " + res);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (Exception e2) {
+                    e2.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
--- codding/src/main/java/net/neoremind/mycode/nio/bio/TimeServer.java
+++ codding/src/main/java/net/neoremind/mycode/nio/bio/TimeServer.java
@@ -5,26 +5,26 @@ import java.net.Socket;
 
 public class TimeServer {
 
-	public static void main(String[] args) {
-		int port = 8080;
-		ServerSocket server = null;
-		try {
-			server = new ServerSocket(port);
-			System.out.println("The time server starts in port : " + port);
-			Socket socket = null;
-			while (true) {
-				socket = server.accept();
-				new Thread(new TimeServerHandler(socket)).start();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				server.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
+    public static void main(String[] args) {
+        int port = 8082;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server starts in port : " + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                server.close();
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+    }
 
 }

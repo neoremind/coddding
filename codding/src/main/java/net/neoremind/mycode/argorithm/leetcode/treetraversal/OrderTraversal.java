b2d7d2e0dba19a8b3819efeacc3a66926d28c3bd
neoremind <notyetfish@hotmail.com>
2016-07-17 15:08:47 +0800

    add preorder

---
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/treetraversal/OrderTraversal.java
@@ -0,0 +1,14 @@
+package net.neoremind.mycode.argorithm.leetcode.treetraversal;
+
+import java.util.List;
+
+import net.neoremind.mycode.argorithm.leetcode.support.TreeNode;
+
+/**
+ * @author zhangxu
+ */
+public interface OrderTraversal {
+
+    List<Integer> traverse(TreeNode root);
+
+}

088e8b8489d06b1842828ef88dfde8dfab914aac
neoremind <notyetfish@hotmail.com>
2016-07-17 15:53:43 +0800

    add bi-tree traversal

---
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/treetraversal/BinaryTreeLevelorderII.java
@@ -0,0 +1,114 @@
+package net.neoremind.mycode.argorithm.leetcode.treetraversal;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import net.neoremind.mycode.argorithm.leetcode.support.TreeNode;
+import net.neoremind.mycode.argorithm.leetcode.support.TreeNodeHelper;
+
+/**
+ * Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right,
+ * level by level from leaf to root).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ * <pre>
+ *     3
+ *    / \
+ *   9  20
+ *  /     \
+ * 15     7
+ * </pre>
+ * return its bottom-up level order traversal as:
+ * <pre>
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ * </pre>
+ *
+ * @author zhangxu
+ */
+public class BinaryTreeLevelorderII {
+
+    public List<List<Integer>> traverse(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>(0);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        List<TreeNode> level = new ArrayList<>(1);
+        level.add(root);
+        levelOrderBottom(level, result);
+        return result;
+    }
+
+    private static void levelOrderBottom(List<TreeNode> level, List<List<Integer>> result) {
+        List<TreeNode> newLevel = new ArrayList<>(level.size() << 1);
+        List<Integer> levelValues = new ArrayList<>(level.size());
+        for (TreeNode treeNode : level) {
+            levelValues.add(treeNode.val);
+            if (treeNode.left != null) {
+                newLevel.add(treeNode.left);
+            }
+            if (treeNode.right != null) {
+                newLevel.add(treeNode.right);
+            }
+        }
+        if (!newLevel.isEmpty()) {
+            levelOrderBottom(newLevel, result);
+        }
+        result.add(levelValues);
+    }
+
+    /**
+     * <pre>
+     *      1
+     *    /   \
+     *   2     3
+     *  / \
+     * 4   5
+     * </pre>
+     * <pre>
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * </pre>
+     * <pre>
+     *           4
+     *         /   \
+     *        2     7
+     *       /     / \
+     *      1     6   9
+     *     / \     \
+     *    8  10     5
+     * </pre>
+     */
+    @Test
+    public void testLevelorder() {
+        testOne("1,2,3,4,5", Lists.newArrayList(4, 5, 2, 3, 1));
+        testOne("1", Lists.newArrayList(1));
+        testOne("4,2,7,1,3,6,9", Lists.newArrayList(1, 3, 6, 9, 2, 7, 4));
+        testOne("4,2,7,1,#,6,9,8,10,#,5", Lists.newArrayList(8, 10, 5, 1, 6, 9, 2, 7, 4));
+    }
+
+    private void testOne(String arr, List<Integer> expected) {
+        TreeNode root = TreeNodeHelper.init(arr);
+        System.out.println("Original tree: " + TreeNodeHelper.preorderTraversal(root));
+        List<List<Integer>> res = traverse(root);
+        List<Integer> res2 = res.stream().flatMap(l -> l.stream()).collect(Collectors.toList());
+        System.out.println("LevelorderII traversal: " + res);
+        assertThat(res2, is(expected));
+    }
+
+}

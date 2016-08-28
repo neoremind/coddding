8089ae2624ea40250bd6ab97e899751da0788287
neoremind <notyetfish@hotmail.com>
2016-08-28 11:17:31 +0800

    add backtracking

--- codding/src/main/java/net/neoremind/mycode/guava/PredicateTest.java
+++ codding/src/main/java/net/neoremind/mycode/guava/PredicateTest.java
@@ -13,52 +13,54 @@ import net.neoremind.mycode.guava.vo.Person;
 
 public class PredicateTest {
 
-	public static void main(String[] args) {
-		List<Person> peopleList = new ArrayList<Person>();
-		peopleList.add(new Person("bowen", 27));
-		peopleList.add(new Person("bob", 20));
-		peopleList.add(new Person("Katy", 18));
-		peopleList.add(new Person("Logon", 24));
+    public static void main(String[] args) {
+        List<Person> peopleList = new ArrayList<Person>();
+        peopleList.add(new Person("bowen", 27));
+        peopleList.add(new Person("bob", 20));
+        peopleList.add(new Person("Katy", 18));
+        peopleList.add(new Person("Logon", 24));
 
-		Collection<Person> oldPeople = Collections2.filter(peopleList, new Predicate<Person>() {
-			public boolean apply(Person person) {
-				return person.getAge() >= 20;
-			}
-		});
+        Collection<Person> oldPeople = Collections2.filter(peopleList, new Predicate<Person>() {
+            public boolean apply(Person person) {
+                return person.getAge() >= 20;
+            }
+        });
 
-		for (Person person : oldPeople) {
-			System.out.println(person);
-		}
-		
-		
-		Collection<Person> nameAndOldPeople = Collections2.filter(peopleList, Predicates.and(nameContains("b"), ageBiggerThan(20)));
+        for (Person person : oldPeople) {
+            System.out.println(person);
+        }
 
-		for (Person person : nameAndOldPeople) {
-			System.out.println(person);
-		}
-		
-		
+        Collection<Person> nameAndOldPeople =
+                Collections2.filter(peopleList, Predicates.and(nameContains("b"), ageBiggerThan(20)));
+        // Java8 lambda
+        Collection<Person> nameAndOldPeople2 = Collections2.filter(peopleList,
+                Predicates.and(p -> p.getName().contains("b"), p -> p.getAge() > 20));
+
+        for (Person person : nameAndOldPeople) {
+            System.out.println(person);
+        }
+
         Predicate<CharSequence> twoDigitsPredicate = Predicates.containsPattern("\\d\\d");
         System.out.println(twoDigitsPredicate.apply("Hello world 40"));
-        
+
         Predicate<Long> elevenInCollectionPredicate = Predicates.in(Arrays.asList(11L, 22L, 33L, 44L));
         System.out.println(elevenInCollectionPredicate.apply(11L));
-	}
+    }
 
-	private static Predicate<Person> nameContains(final String str) {
-		return new Predicate<Person>() {
-			public boolean apply(Person person) {
-				return person.getName().contains(str);
-			}
-		};
-	}
+    private static Predicate<Person> nameContains(final String str) {
+        return new Predicate<Person>() {
+            public boolean apply(Person person) {
+                return person.getName().contains(str);
+            }
+        };
+    }
 
-	private static Predicate<Person> ageBiggerThan(final int age) {
-		return new Predicate<Person>() {
-			public boolean apply(Person person) {
-				return person.getAge() >= age;
-			}
-		};
-	}
+    private static Predicate<Person> ageBiggerThan(final int age) {
+        return new Predicate<Person>() {
+            public boolean apply(Person person) {
+                return person.getAge() >= age;
+            }
+        };
+    }
 
 }
---
+++ codding/src/main/java/net/neoremind/mycode/guava/vo/PersonDto.java
@@ -0,0 +1,52 @@
+package net.neoremind.mycode.guava.vo;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+public class PersonDto {
+
+    private int age;
+
+    private String name;
+
+    public PersonDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("age", age).add("name", name).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(age, name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof PersonDto) {
+            PersonDto that = (PersonDto) obj;
+            return Objects.equal(age, that.age) && Objects.equal(name, that.name);
+        }
+        return false;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}

ca3b48ea004fcb7385f6edf63b28595bd1d119f6
neoremind <notyetfish@hotmail.com>
2016-09-02 17:32:37 +0800

    add partition list

---
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/PartitionList.java
@@ -0,0 +1,38 @@
+package net.neoremind.mycode.argorithm.leetcode;
+
+import net.neoremind.mycode.argorithm.leetcode.support.ListNode;
+
+/**
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or
+ * equal to x.
+ * <p>
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ * <p>
+ * For example,
+ * Given 1->4->3->2->5->2 and x = 3,
+ * return 1->2->2->4->3->5.
+ *
+ * @author zhangxu
+ * @see https://leetcode.com/problems/partition-list/
+ */
+public class PartitionList {
+
+    public ListNode partition(ListNode head, int x) {
+        ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);  //dummy heads of the 1st and 2nd queues
+        ListNode curr1 = dummy1, curr2 = dummy2;      //current tails of the two queues;
+        while (head != null) {
+            if (head.val < x) {
+                curr1.next = head;
+                curr1 = head;
+            } else {
+                curr2.next = head;
+                curr2 = head;
+            }
+            head = head.next;
+        }
+        curr2.next = null;          //important! avoid cycle in linked list. otherwise u will get TLE.
+        curr1.next = dummy2.next;
+        return dummy1.next;
+    }
+
+}
--- codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/README.md
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/README.md
@@ -691,6 +691,29 @@ return IntStream.range(0, (int) Math.pow(2, n))
         .collect(Collectors.toList());
 ```
 
+### [86. Partition List](https://leetcode.com/problems/partition-list/)
+
+M, Linked List Two Pointers
+
+curr2.next = null;非常重要，避免环的出现！
+
+```
+ListNode dummy1 = new ListNode(0), dummy2 = new ListNode(0);  //dummy heads of the 1st and 2nd queues
+ListNode curr1 = dummy1, curr2 = dummy2;      //current tails of the two queues;
+while (head != null)
+    if (head.val < x) {
+        curr1.next = head;
+        curr1 = head;
+    } else {
+        curr2.next = head;
+        curr2 = head;
+    }
+    head = head.next;
+curr2.next = null;          //important! avoid cycle in linked list. otherwise u will get TLE.
+curr1.next = dummy2.next;
+return dummy1.next;
+```
+
 ### [83. Remove Duplicates from Sorted List](https://leetcode.com/problems/remove-duplicates-from-sorted-list/)
 
 E, Linked list

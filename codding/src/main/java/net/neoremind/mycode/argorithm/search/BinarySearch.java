f7fed38a0a2fc5a21bd4b8ddecd9c8d53e8856c0
neoremind <notyetfish@hotmail.com>
2016-09-04 08:55:20 +0800

    refine binary search

--- codding/src/main/java/net/neoremind/mycode/argorithm/search/BinarySearch.java
+++ codding/src/main/java/net/neoremind/mycode/argorithm/search/BinarySearch.java
@@ -85,6 +85,22 @@ public class BinarySearch {
         return -1;
     }
 
+    private int searchLoop2(int[] arr, int start, int end, int target) {
+        while (start < end) {
+            int mid = (start + end) >>> 1;
+            int midVal = arr[mid];
+
+            if (midVal < target) {
+                start = mid + 1;
+            } else if (midVal > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return arr[start] == target ? start : -1;
+    }
+
     public static void main(String[] args) {
         /**
          * 验证start + end >>> 2 和 (start + end) / 2 相等

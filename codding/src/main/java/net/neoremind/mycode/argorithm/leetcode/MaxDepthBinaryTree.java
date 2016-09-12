64a6e536849049b664adcd94925627054056b5bd
neoremind <notyetfish@hotmail.com>
2016-09-12 15:11:30 +0800

    add max depth

--- codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/MaxDepthBinaryTree.java
+++ codding/src/main/java/net/neoremind/mycode/argorithm/leetcode/MaxDepthBinaryTree.java
@@ -8,7 +8,7 @@ import net.neoremind.mycode.argorithm.leetcode.support.TreeNodeHelper;
 
 /**
  * Given a binary tree, find its maximum depth.
- * <p/>
+ * <p>
  * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
  */
 public class MaxDepthBinaryTree {
@@ -17,32 +17,23 @@ public class MaxDepthBinaryTree {
         if (root == null) {
             return 0;
         }
-        int leftDepth;
-        int rightDepth;
-        leftDepth = maxDepth(root.left) + 1;
-        rightDepth = maxDepth(root.right) + 1;
-
-        if (leftDepth >= rightDepth) {
-            return leftDepth;
-        } else {
-            return rightDepth;
-        }
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
     public static void main(String[] args) {
-        int[] arr = new int[]{4, 2, 7, 1, 3, 6, 9};
+        int[] arr = new int[] {4, 2, 7, 1, 3, 6, 9};
         TreeNode root = TreeNodeHelper.init(arr);
         int maxDepth = maxDepth(root);
         System.out.println(maxDepth);
         assertThat(maxDepth, is(3));
 
-        arr = new int[]{4, 2, 7, 1, 3, 6, 9, 11};
+        arr = new int[] {4, 2, 7, 1, 3, 6, 9, 11};
         root = TreeNodeHelper.init(arr);
         maxDepth = maxDepth(root);
         System.out.println(maxDepth);
         assertThat(maxDepth, is(4));
 
-        arr = new int[]{0};
+        arr = new int[] {0};
         root = TreeNodeHelper.init(arr);
         maxDepth = maxDepth(root);
         System.out.println(maxDepth);
